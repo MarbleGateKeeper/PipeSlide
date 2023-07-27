742293b04e7395eab36cd71a291acb02da77140b
MarbleGate <marblegatekeeper@outlook.com>
2023-07-28 06:18:46 +0800

    Remove unused classes

--- src/main/java/plus/dragons/pipeslide/PipeSlideClient.java
+++ src/main/java/plus/dragons/pipeslide/PipeSlideClient.java
@@ -1,26 +1,17 @@
 package plus.dragons.pipeslide;
 
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import plus.dragons.pipeslide.entry.ModBlockEntities;
 import plus.dragons.pipeslide.entry.ModEntityTypes;
 import plus.dragons.pipeslide.entry.ModItems;
-import plus.dragons.pipeslide.foundation.client.model.AllBlockPartials;
-import plus.dragons.pipeslide.foundation.client.model.PartialModel;
 
 public class PipeSlideClient {
     public PipeSlideClient() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(ModBlockEntities::registerRenderer);
         modEventBus.addListener(ModItems::registerCreativeTab);
-        modEventBus.addListener(PipeSlideClient::clientInit);
-        modEventBus.addListener(PartialModel::onModelRegistry);
-        modEventBus.addListener(PartialModel::onModelBake);
         modEventBus.addListener(ModEntityTypes::registerRenderer);
     }
 
-    public static void clientInit(final FMLClientSetupEvent event) {
-        AllBlockPartials.init();
-    }
 }
